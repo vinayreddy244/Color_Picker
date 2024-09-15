c58649ea61cae8b81288d281efc8fbb97fc749e7
Vinay Satya Anand Reddy <83955648+vinayreddy244@users.noreply.github.com>
2024-09-15 19:01:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,252 @@
+function bg_my_red() {
+    document.getElementById("text-box").style.backgroundColor = "red";
+}
+
+function bg_my_blue() {
+    document.getElementById("text-box").style.backgroundColor = "blue";
+}
+
+function bg_my_pink() {
+    document.getElementById("text-box").style.backgroundColor = "pink";
+}
+
+function bg_my_brown() {
+    document.getElementById("text-box").style.backgroundColor = "brown";
+}
+
+function bg_my_yellow() {
+    document.getElementById("text-box").style.backgroundColor = "yellow";
+}
+
+function bg_my_purple() {
+    document.getElementById("text-box").style.backgroundColor = "purple";
+}
+
+function bg_my_black() {
+    document.getElementById("text-box").style.backgroundColor = "black";
+}
+
+function bg_my_darkolivegreen() {
+    document.getElementById("text-box").style.backgroundColor = "darkolivegreen";
+}
+
+function bg_my_green() {
+    document.getElementById("text-box").style.backgroundColor = "orange";
+}
+
+function bg_my_gray() {
+    document.getElementById("text-box").style.backgroundColor = "gray";
+}
+
+function bg_my_darkslategray() {
+    document.getElementById("text-box").style.backgroundColor = "darkslategray";
+}
+
+function bg_my_lightgreen() {
+    document.getElementById("text-box").style.backgroundColor = "lightgreen";
+}
+
+function bg_my_maroon() {
+    document.getElementById("text-box").style.backgroundColor = "maroon";
+}
+
+function bg_my_lime() {
+    document.getElementById("text-box").style.backgroundColor = "lime";
+}
+
+function bg_my_wheat() {
+    document.getElementById("text-box").style.backgroundColor = "wheat";
+}
+
+function bg_my_goldenrod() {
+    document.getElementById("text-box").style.backgroundColor = "goldenrod";
+}
+
+function bg_my_salmon() {
+    document.getElementById("text-box").style.backgroundColor = "salmon";
+}
+
+function bg_my_chocolate() {
+    document.getElementById("text-box").style.backgroundColor = "chocolate";
+}
+
+function bg_my_darkblue() {
+    document.getElementById("text-box").style.backgroundColor = "darkblue";
+}
+
+function bg_my_springgreen() {
+    document.getElementById("text-box").style.backgroundColor = "springgreen";
+}
+
+function bg_my_turquoise() {
+    document.getElementById("text-box").style.backgroundColor = "turquoise";
+}
+
+function bg_my_aqua() {
+    document.getElementById("text-box").style.backgroundColor = "aqua";
+}
+
+function bg_my_crimson() {
+    document.getElementById("text-box").style.backgroundColor = "crimson";
+}
+
+function bg_my_silver() {
+    document.getElementById("text-box").style.backgroundColor = "silver";
+}
+
+function bg_my_darkred() {
+    document.getElementById("text-box").style.backgroundColor = "darkred";
+}
+
+function bg_my_navy() {
+    document.getElementById("text-box").style.backgroundColor = "navy";
+}
+
+function bg_my_orchid() {
+    document.getElementById("text-box").style.backgroundColor = "orchid";
+}
+
+function bg_my_teal() {
+    document.getElementById("text-box").style.backgroundColor = "teal";
+}
+
+function bg_my_blueviolet() {
+    document.getElementById("text-box").style.backgroundColor = "blueviolet";
+}
+
+function bg_my_palegreen() {
+    document.getElementById("text-box").style.backgroundColor = "palegreen";
+}
+
+
+
+
+function f_my_red() {
+    document.getElementById("input1").style.color = "red";
+}
+
+function f_my_blue() {
+    document.getElementById("input1").style.color = "blue";
+}
+
+function f_my_pink() {
+    document.getElementById("input1").style.color = "pink";
+}
+
+function f_my_brown() {
+    document.getElementById("input1").style.color = "brown";
+}
+
+function f_my_yellow() {
+    document.getElementById("input1").style.color = "yellow";
+}
+
+function f_my_purple() {
+    document.getElementById("input1").style.color = "purple";
+}
+
+function f_my_black() {
+    document.getElementById("input1").style.color = "black";
+}
+
+function f_my_darkolivegreen() {
+    document.getElementById("input1").style.color = "darkolivegreen";
+}
+
+function f_my_green() {
+    document.getElementById("input1").style.color = "orange";
+}
+
+function f_my_gray() {
+    document.getElementById("input1").style.color = "gray";
+}
+
+function f_my_darkslategray() {
+    document.getElementById("input1").style.color = "darkslategray";
+}
+
+function f_my_lightgreen() {
+    document.getElementById("input1").style.color = "lightgreen";
+}
+
+function f_my_maroon() {
+    document.getElementById("input1").style.color = "maroon";
+}
+
+function f_my_lime() {
+    document.getElementById("input1").style.color = "lime";
+}
+
+function f_my_wheat() {
+    document.getElementById("input1").style.color = "wheat";
+}
+
+function f_my_goldenrod() {
+    document.getElementById("input1").style.color = "goldenrod";
+}
+
+function f_my_salmon() {
+    document.getElementById("input1").style.color = "salmon";
+}
+
+function f_my_chocolate() {
+    document.getElementById("input1").style.color = "chocolate";
+}
+
+function f_my_darkblue() {
+    document.getElementById("input1").style.color = "darkblue";
+}
+
+function f_my_springgreen() {
+    document.getElementById("input1").style.color = "springgreen";
+}
+
+function f_my_turquoise() {
+    document.getElementById("input1").style.color = "turquoise";
+}
+
+function f_my_aqua() {
+    document.getElementById("input1").style.color = "aqua";
+}
+
+function f_my_crimson() {
+    document.getElementById("input1").style.color = "crimson";
+}
+
+function f_my_silver() {
+    document.getElementById("input1").style.color = "silver";
+}
+
+function f_my_darkred() {
+    document.getElementById("input1").style.color = "darkred";
+}
+
+function f_my_navy() {
+    document.getElementById("input1").style.color = "navy";
+}
+
+function f_my_orchid() {
+    document.getElementById("input1").style.color = "orchid";
+}
+
+function f_my_teal() {
+    document.getElementById("input1").style.color = "teal";
+}
+
+function f_my_blueviolet() {
+    document.getElementById("input1").style.color = "blueviolet";
+}
+
+function f_my_palegreen() {
+    document.getElementById("input1").style.color = "palegreen";
+}
+
+function reset()
+{
+    document.getElementById("text-box").style.backgroundColor="";
+    document.getElementById("input1").style.color="";
+    document.getElementById("input1").value="";
+
+}
+
+
